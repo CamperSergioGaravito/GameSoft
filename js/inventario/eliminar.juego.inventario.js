4a9b5c2a9b660d53082404c919518d075e402383
CamperSergiogaravito <camper.sergiog@gmail.com>
2023-10-23 18:30:03 -0500

    v0.0.3

--- js/inventario/eliminar.juego.inventario.js
+++ js/inventario/eliminar.juego.inventario.js
@@ -1,4 +1,19 @@
+import { guardarStorage, obtenerItem } from "../storage/storage.acciones.js";
+
 export function eliminarJuego(e) {
-    console.log(e.target.offsetParent.querySelector('#idJuego').textcontent);
-    e.target.offsetParent.remove();
+    console.log(e.target.offsetParent.querySelector('#idJuego').textContent);
+    const inventario = obtenerItem('inventario');
+    console.log(`eliminar.juego.inventario.js :: eliminarJuego :: inventario -> ${inventario} `,inventario)
+
+    for(let x=0;x<inventario.length;x++) {
+        if(inventario[x].id === e.target.offsetParent.querySelector('#idJuego').textContent) {
+            inventario.splice(x,1);
+            guardarStorage('inventario',inventario);
+            e.target.offsetParent.remove();
+            return
+        }
+        else  {
+
+        }
+    }
 }
